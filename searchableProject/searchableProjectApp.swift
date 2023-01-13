477c0d093b3cc6ac6bdaa2e08528e2c54a79c05b
Tedtya Rady <tedtyarady@gmail.com>
2023-01-13 16:19:39 +1100

    Initial Commit

---
+++ searchableProject/searchableProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  searchableProjectApp.swift
+//  searchableProject
+//
+//  Created by Tedtya rady on 13/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct searchableProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
